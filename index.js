b5866e61f4abdec65828532a68bf2001cb3084d7
Shubhisingh921 <shubhis921@gmail.com>
2024-08-22 20:21:54 +0530

    adding files

---
+++ index.js
@@ -0,0 +1,18 @@
+const express=require("express");
+const enterprise=require("./MOCK_DATA.json");
+const app=express();
+const PORT=8000;
+//ROUTES
+app.get("/api/enterprise",(req, res)=> {
+    return res.json(enterprise)
+});
+
+app.get("/enterprise",(req, res)=>{
+    const html=`
+    <ul>
+    ${enterprise.map((enterprise)=>`<li>${enterprise.enterprise_name}</li>`
+    )}
+    </ul>`;
+    res.send(html);
+});
+app.listen(PORT,()=>console.log(`Server Started at PORT:${PORT}`));
