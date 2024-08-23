ce92fa57b9f3ae990ee3327af39137ba71b9b753
Shubhisingh921 <shubhis921@gmail.com>
2024-08-23 17:14:49 +0530

    upadted

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express=require("express");
+const cors = require('cors');
 const enterprise=require("./MOCK_DATA.json");
 const app=express();
 const PORT=8000;
+app.use(cors());
 //ROUTES
 app.get("/",(req, res)=> {
     return res.json(enterprise)
