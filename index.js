54c6d8b5fbf727f09a6c5bd1bf761b6158ea51f6
Shubhisingh921 <shubhis921@gmail.com>
2024-08-23 12:00:54 +0530

    updating json

--- index.js
+++ index.js
@@ -7,12 +7,4 @@ app.get("/",(req, res)=> {
     return res.json(enterprise)
 });
 
-app.get("/enterprise",(req, res)=>{
-    const html=`
-    <ul>
-    ${enterprise.map((enterprise)=>`<li>${enterprise.enterprise_name}</li>`
-    )}
-    </ul>`;
-    res.send(html);
-});
 app.listen(PORT,()=>console.log(`Server Started at PORT:${PORT}`));
